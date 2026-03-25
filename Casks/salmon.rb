658720d59d512b7e05e9b2a5ef985b8c98a4b438
Srisanth <srisanth.p.a@gmail.com>
2026-03-26 03:23:50 +0530

    Update salmon.rb

--- Casks/salmon.rb
+++ Casks/salmon.rb
@@ -1,6 +1,6 @@
 cask "salmon" do
   version "1.0.0"
-  sha256 "ff5b89c8a5e811be6f327ab20898225db33fc470780a795febb5679e48d93851"
+  sha256 "dfb5f5d36b7cad49c8ef34446cd08c2bd3bb05a45a8cec21bae6cc3f3b6c8dc4"
 
   url "https://github.com/santh-cpu/salmon/releases/download/v#{version}/Salmon.app.zip"
   name "Salmon"

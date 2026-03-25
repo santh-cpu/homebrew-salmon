7289d937e6b0dc43293c547b427cede8aec4d435
santh-cpu <srisanth.p.a@gmail.com>
2026-03-25 20:26:59 +0530

    added cask application rb

---
+++ Casks/salmon.rb
@@ -0,0 +1,11 @@
+cask "salmon" do
+  version "1.0.0"
+  sha256 "PASTE_APP_ZIP_SHA256_HERE"
+
+  url "https://github.com/santh-cpu/salmon/releases/download/v1.0.0/Salmon.app.zip"
+  name "Salmon"
+  desc "Offline terminal guitar tuner with real-time mic input"
+  homepage "https://github.com/santh-cpu/salmon"
+
+  app "Salmon.app"
+end
